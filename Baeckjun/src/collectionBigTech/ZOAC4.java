0533425f7321441ee7247cd0fd8b504c8743e433
sejin <sejin3319@ex-em.com>
2023-09-08 19:43:54 +0900

    zoca4

---
+++ Baeckjun/src/collectionBigTech/ZOAC4.java
@@ -0,0 +1,29 @@
+package collectionBigTech;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class ZOAC4 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer input = new StringTokenizer(br.readLine(), " ");
+//        StringTokenizer input = new StringTokenizer("5 4 1 1", " ");
+//        StringTokenizer st = new StringTokenizer(input, " ");
+
+        int H = Integer.valueOf(input.nextToken());
+        int W = Integer.valueOf(input.nextToken());
+        int N = Integer.valueOf(input.nextToken());
+        int M = Integer.valueOf(input.nextToken());
+
+        int height = (H - 1) / (N + 1) + 1;
+        int width = (W - 1) / (M + 1) + 1;
+
+        System.out.print(height * width);
+
+    }
+ }
+
+
